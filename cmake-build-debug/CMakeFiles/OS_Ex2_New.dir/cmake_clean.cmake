6d0e002e97f8a72c6c6fb5ad9fb72081a335611a
Ran Hadar <52695553+RanHadar@users.noreply.github.com>
2020-05-02 10:55:46 +0300

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/OS_Ex2_New.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OS_Ex2_New.dir/Scheduler.cpp.obj"
+  "CMakeFiles/OS_Ex2_New.dir/Thread.cpp.obj"
+  "CMakeFiles/OS_Ex2_New.dir/main.cpp.obj"
+  "CMakeFiles/OS_Ex2_New.dir/uthreads.cpp.obj"
+  "OS_Ex2_New.exe"
+  "OS_Ex2_New.exe.manifest"
+  "OS_Ex2_New.pdb"
+  "libOS_Ex2_New.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OS_Ex2_New.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
